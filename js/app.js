6a754d8941292e521f7f6c90e8673810224a2c23
Omar Noyola <omis1296@gmail.com>
2022-06-27 19:26:06 -0500

    :rocket: Inicio Profuturo

---
+++ js/app.js
@@ -0,0 +1,54 @@
+import { App } from "./clases/App.js";
+import { modoDesarrollador, tiempo, redimensionar } from "./funciones.js";
+import {btnAdelante, btnAtras, btnMenu, btnCerrar, btnAudio, btnRecargar} from './selectores.js';
+import { registrarTiempo, cerrarConexion } from './conexion.js';
+
+//Instanciar App e iniciarla------------------------------------------------------
+export const aplicacion = new App;
+aplicacion.iniciar();
+
+// Funciones de los botones de navegación-----------------------------------------
+btnAdelante.click(function(){
+    aplicacion.nextPag();
+});
+
+btnAtras.click(function(){
+    aplicacion.prevPag();
+});
+
+btnMenu.click(function(){
+    aplicacion.desplegarMenu();
+});
+
+btnCerrar.click(function(){
+    aplicacion.cerrarMenu();
+});
+
+btnAudio.click(function(){
+    aplicacion.silenciarOPrender();
+});
+
+btnRecargar.click(function(){
+    aplicacion.recargarPag();
+});
+
+//Al cerrar el curso
+window.onunload = function(){
+    registrarTiempo(tiempo);
+    cerrarConexion();
+}
+
+// Modo desarrollador-------------------------------------------------------------
+modoDesarrollador();
+
+//Redimensionar tamaño------------------------------------------------------------
+redimensionar();
+let intervalo; 
+$(window).resize(function(){
+    clearTimeout(intervalo);
+    intervalo = setTimeout(escalar, 100);
+});
+
+function escalar(){
+    aplicacion.recargarPag();
+}
